c5b8ec1db0a964bd0262d31c22dc790ea0bf1be2
Николай Гребенюк <Yanukhous@ukr.net>
2021-07-27 12:52:57 +0300

    Updates

--- src.3691a010.js
+++ src.3691a010.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-hw-10-food-src/src.bafc6704.js.map
+//# sourceMappingURL=/goit-js-hw-10-food-src/src.3691a010.js.map
